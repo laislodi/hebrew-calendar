a6174c7ce4ae9716dfc3274457d38822ab885853
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-09-13 20:06:22 -0600

    Adjust vite.config to add base and test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/portfolio/', // important for GitHub Pages
+  test: {
+    globals: true,
+    setupFiles: 'src/setupTests.js',
+    environment: 'jsdom',
+  }
+});
