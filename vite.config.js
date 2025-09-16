dc5f30918628bfe5033df8d4a1e38f77104e9e43
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-09-15 20:33:01 -0600

    Fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio', // important for GitHub Pages
+  base: '/hebrew-calendar', // important for GitHub Pages
   test: {
     globals: true,
     setupFiles: 'src/setupTests.js',
